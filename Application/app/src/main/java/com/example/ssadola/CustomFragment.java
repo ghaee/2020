4ea7f2336d7a79d28891ebe2d9ddfbd23d45a82e
ghaee <ghaee200@gmali.com>
2020-04-17 18:57:37 +0900

    More Activity

--- Application/app/src/main/java/com/example/ssadola/CustomFragment.java
+++ Application/app/src/main/java/com/example/ssadola/CustomFragment.java
@@ -1,16 +1,21 @@
 package com.example.ssadola;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import yalantis.com.sidemenu.interfaces.ScreenShotable;
 import yalantis.com.sidemenu.util.ViewAnimator;
@@ -34,12 +39,14 @@ public class CustomFragment extends Fragment implements ScreenShotable {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.containerView = view.findViewById(R.id.container);
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         res = getArguments().getInt(Integer.class.getName());
+
     }
 
     @Override
@@ -50,6 +57,15 @@ public class CustomFragment extends Fragment implements ScreenShotable {
         mImageView.setClickable(true);
         mImageView.setFocusable(true);
         mImageView.setImageResource(res);*/
+
+        FloatingActionButton fab = rootView.findViewById(R.id.Floating1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent result = new Intent(getActivity(),ResultActivity.class);
+                startActivity(result);
+            }
+        });
         return rootView;
     }
     @Override
